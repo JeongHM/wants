32e277b28b362087275916c8279532d8bd177ef7
JeongHM <kjhm0607@gmail.com>
2021-01-09 01:27:00 +0900

    [fix] change regex

--- validators/company.py
+++ validators/company.py
@@ -3,12 +3,12 @@ from marshmallow.validate import Length, Regexp
 
 
 class CompanyPostSchema(Schema):
-    company_tag_regex = "[a-zA-Z0-9가-힇ㄱ-ㅎㅏ-ㅣぁ-ゔァ-ヴー々〆〤一-龥]+_[0-9]+"
+    company_tag_regex = "^[tag,태그,タグ]+_[0-9]+$"
     company_name = fields.Str(required=True, allow_none=False, validate=Length(min=1))
     company_tag = fields.Str(required=True, allow_none=False, validate=Regexp(company_tag_regex))
 
 
 class CompanyDeleteSchema(Schema):
-    company_tag_regex = "[a-zA-Z0-9가-힇ㄱ-ㅎㅏ-ㅣぁ-ゔァ-ヴー々〆〤一-龥]+_[0-9]+"
+    company_tag_regex = "^[tag,태그,タグ]+_[0-9]+$"
     company_name = fields.Str(required=True, allow_none=False, validate=Length(min=1))
     company_tag = fields.Str(required=True, allow_none=False, validate=Regexp(company_tag_regex))
